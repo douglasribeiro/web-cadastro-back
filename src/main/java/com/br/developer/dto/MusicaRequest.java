d96482f445e32245ff14c1492b19af3422eb1106
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2026-05-12 16:28:18 -0300

    Validaçoes no cadastro de musicas

--- src/main/java/com/br/developer/dto/MusicaRequest.java
+++ src/main/java/com/br/developer/dto/MusicaRequest.java
@@ -3,11 +3,15 @@ package com.br.developer.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MusicaRequest {
 	
 	private Long id;
